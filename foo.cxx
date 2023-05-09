94367b90a272d37b9ebb295c0d6f4705941f2547
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2023-05-09 11:53:01 +0200

    add dcmtk

--- foo.cxx
+++ foo.cxx
@@ -1,15 +1,15 @@
 #include "public.h"
 
-//#include "dcmtk/config/osconfig.h" /* make sure OS specific configuration is included first */
-//#include "dcmtk/dcmdata/dctk.h"
-//#include "dcmtk/oflog/spi/logevent.h"  // dcmtk::log4cplus::spi::InternalLoggingEvent
+#include "dcmtk/config/osconfig.h" /* make sure OS specific configuration is included first */
+#include "dcmtk/dcmdata/dctk.h"
+#include "dcmtk/oflog/spi/logevent.h"  // dcmtk::log4cplus::spi::InternalLoggingEvent
 
 
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
 #include <boost/filesystem/path.hpp>
 
-//dcmtk::log4cplus::Logger rootLogger = dcmtk::log4cplus::Logger::getRoot();
+dcmtk::log4cplus::Logger rootLogger = dcmtk::log4cplus::Logger::getRoot();
 
 
 int myfoo() { return 42; }
